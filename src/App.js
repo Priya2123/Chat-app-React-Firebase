e3be254aec2389818a0cab9b0c9a16d7f881f31d
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-10-01 11:03:08 +0530

    basic signin signout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
+firebase.initializeApp({
+  apiKey: "AIzaSyBnwJ9cqNoC759KzJXSiIlOF9rqxypotV4",
+  authDomain: "chat-23af4.firebaseapp.com",
+  databaseURL: "https://chat-23af4.firebaseio.com",
+  projectId: "chat-23af4",
+  storageBucket: "chat-23af4.appspot.com",
+  messagingSenderId: "209339788556",
+  appId: "1:209339788556:web:6f7f06df0ddcac322d6d29",
+  measurementId: "G-EK8EEZG86D",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+//to check if the user is logged in or not
+const [user] = useAuthState(auth);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
 
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new.firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  function SignOut() {
+    return (
+      auth.currentUser && (
+        <button onClick={() => auth.SignOut()}>Sign Out</button>
+      )
+    );
+  }
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
 export default App;
